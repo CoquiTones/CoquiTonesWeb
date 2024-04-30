31ad92fc39771bbd4e0027123ad495f592314812
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-04-30 15:08:44 -0400

    added buttons to control ticks easier when ranges are too close

--- Frontend/src/components/SoundAnalysisTools/SpectrogramControls.jsx
+++ Frontend/src/components/SoundAnalysisTools/SpectrogramControls.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Typography from '@mui/material/Typography';
@@ -10,8 +10,12 @@ import Select from '@mui/material/Select';
 import FileUpload from '../shared/FileUpload';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box'; // Import Box component
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 import StyledSlider from '../shared/StyledSlider';
+
 function ValueLabelComponent(props) {
     const { children, value } = props;
 
@@ -27,10 +31,16 @@ ValueLabelComponent.propTypes = {
     value: PropTypes.number.isRequired,
 };
 
-
-
-
 export default function SpectrogramControls({ setAudioFile, type, setType, colorscale, setColorscale, xrange, setXrange, yrange, setYrange }) {
+
+    const handleXRangeChange = (newValue) => {
+        setXrange(newValue);
+    };
+
+    const handleYRangeChange = (newValue) => {
+        setYrange(newValue);
+    };
+
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
             <FileUpload setAudioFile={setAudioFile} />
@@ -69,40 +79,59 @@ export default function SpectrogramControls({ setAudioFile, type, setType, color
                 </Select>
             </FormControl>
 
-            <Typography gutterBottom >
+            <Typography gutterBottom>
                 Time (s) Range
             </Typography>
-            <StyledSlider
-                sx={
-                    {
-                        marginTop: 4
-                    }
-                }
-                defaultValue={[0, 300]}
-                value={xrange.map(value => value.toFixed(1))}
-                onChange={(event) => setXrange(event.target.value)}
-                valueLabelDisplay='on'
-                min={0}
-                max={300}
-            />
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <IconButton onClick={() => handleXRangeChange([xrange[0] - 1, xrange[1]])}>
+                    <RemoveIcon />
+                </IconButton>
+                <IconButton onClick={() => handleXRangeChange([xrange[0] + 1, xrange[1]])}>
+                    <AddIcon />
+                </IconButton>
+                <StyledSlider
+                    sx={{ marginTop: 4, flexGrow: 1 }}
+                    defaultValue={[0, 300]}
+                    value={xrange}
+                    onChange={(event, newValue) => handleXRangeChange(newValue)}
+                    valueLabelDisplay="on"
+                    min={0}
+                    max={300}
+                />
+                <IconButton onClick={() => handleXRangeChange([xrange[0], xrange[1] - 1])}>
+                    <RemoveIcon />
+                </IconButton>
+                <IconButton onClick={() => handleXRangeChange([xrange[0], xrange[1] + 1])}>
+                    <AddIcon />
+                </IconButton>
+            </Box>
 
             <Typography gutterBottom>
                 Frequency (Hz) Range
             </Typography>
-            <StyledSlider
-                sx={
-                    {
-                        marginTop: 4
-                    }
-                }
-                defaultValue={[0, 10000]}
-                value={yrange.map(value => value.toFixed(1))}
-                onChange={(event) => setYrange(event.target.value)}
-                valueLabelDisplay='on'
-                min={0}
-                max={10000}
-
-            />
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <IconButton onClick={() => handleYRangeChange([yrange[0] - 1, yrange[1]])}>
+                    <RemoveIcon />
+                </IconButton>
+                <IconButton onClick={() => handleYRangeChange([yrange[0] + 1, yrange[1]])}>
+                    <AddIcon />
+                </IconButton>
+                <StyledSlider
+                    sx={{ marginTop: 4, flexGrow: 1 }}
+                    defaultValue={[0, 10000]}
+                    value={yrange}
+                    onChange={(event, newValue) => handleYRangeChange(newValue)}
+                    valueLabelDisplay="on"
+                    min={0}
+                    max={10000}
+                />
+                <IconButton onClick={() => handleYRangeChange([yrange[0], yrange[1] - 1])}>
+                    <RemoveIcon />
+                </IconButton>
+                <IconButton onClick={() => handleYRangeChange([yrange[0], yrange[1] + 1])}>
+                    <AddIcon />
+                </IconButton>
+            </Box>
         </Box>
     );
 }
