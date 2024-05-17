0c32b629641971edb9ac1bbdbcd6d3a3f6e64d67
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-05-17 13:00:12 -0400

    filter by selection once audio is already playing

--- Frontend/src/components/SoundAnalysisTools/SoundPlayer.jsx
+++ Frontend/src/components/SoundAnalysisTools/SoundPlayer.jsx
@@ -1,108 +1,97 @@
 import React, { useEffect, useRef, useState } from "react";
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import { Button } from "@mui/material";
 import theme from "../shared/Theme";
 
-export default function SoundPlayer({ file, setCurrentTime, yrange, xrange, currentTime }) {
-    const [playing, setPlaying] = useState(false);
-    const [audioUrl, setAudioUrl] = useState(null);
-    useEffect(() => {
-
-        if (file) {
-            const url = URL.createObjectURL(file)
-            setAudioUrl(url)
-        }
-    }, [file])
-
-
-    const [source, setSource] = useState(null)
-    const [bandpassFilter, setBandPassFilter] = useState(null)
-    const [audioContext, setAudioContext] = useState(null)
-    const audioElementRef = useRef(null);
-
-
-    function filterAudio() {
-
-
-        const centerFrequency = (yrange[0] + yrange[1]) / 2; // Set center frequency
-        const qualityFactor = centerFrequency / (yrange[1] - yrange[0])
-        bandpassFilter.type = 'bandpass'; // Set filter type to bandpass
-        bandpassFilter.frequency.value = centerFrequency;
-        bandpassFilter.Q.value = qualityFactor; // Set quality factor
-
-
+export default function SoundPlayer({
+  file,
+  setCurrentTime,
+  yrange,
+  xrange,
+  currentTime,
+}) {
+  const [playing, setPlaying] = useState(false);
+  const [audioUrl, setAudioUrl] = useState(null);
+  useEffect(() => {
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setAudioUrl(url);
     }
+  }, [file]);
 
+  const [source, setSource] = useState(null);
+  const [bandpassFilter, setBandPassFilter] = useState(null);
+  const [audioContext, setAudioContext] = useState(null);
+  const audioElementRef = useRef(null);
 
-    useEffect(() => {
+  function filterAudio() {
+    const centerFrequency = (yrange[0] + yrange[1]) / 2; // Set center frequency
+    const qualityFactor = centerFrequency / (yrange[1] - yrange[0]);
+    bandpassFilter.type = "bandpass"; // Set filter type to bandpass
+    bandpassFilter.frequency.value = centerFrequency;
+    bandpassFilter.Q.value = qualityFactor; // Set quality factor
 
+    console.log("filtered audio!");
+  }
 
-        if (audioUrl && audioElementRef.current && source) {
-            filterAudio()
-        }
-    }, [yrange, audioUrl])
+  useEffect(() => {
+    if (audioUrl && audioElementRef.current && source) {
+      filterAudio();
+    }
+  }, [yrange, audioUrl]);
 
+  const handleTimeUpdate = (event) => {
+    setCurrentTime(event.target.currentTime);
+  };
 
+  useEffect(() => {
+    if (currentTime > xrange[1]) {
+      audioElementRef.current.currentTime = xrange[0];
+    }
+  }, [currentTime, xrange]);
 
-    const handleTimeUpdate = (event) => {
-        setCurrentTime(event.target.currentTime);
-    };
+  const handleClick = () => {
+    // async react bullshit
+    setPlaying(!playing);
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    setAudioContext(audioCtx);
+  };
+  useEffect(() => {
+    if (audioContext) {
+      const audioSrc = audioContext.createMediaElementSource(
+        audioElementRef.current
+      );
+      setSource(audioSrc);
+      const biquadFilter = audioContext.createBiquadFilter();
+      setBandPassFilter(biquadFilter);
+    }
+  }, [audioContext]);
 
-    useEffect(() => {
+  useEffect(() => {
+    if (source) {
+      source.connect(bandpassFilter);
+      bandpassFilter.connect(audioContext.destination);
+    }
+  }, [source, bandpassFilter]);
 
-        if ((currentTime > xrange[1])) {
+  return (
+    <ThemeProvider theme={theme}>
+      {audioUrl && !playing && (
+        <Button variant="condensed" onClick={() => handleClick()}>
+          Play Sample
+        </Button>
+      )}
 
-            audioElementRef.current.currentTime = xrange[0]
-        }
-
-
-    }, [currentTime, xrange])
-
-    const handleClick = () => {
-        // async react bullshit 
-        setPlaying(!playing);
-        const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        setAudioContext(audioCtx)
-
-
-
-    };
-    useEffect(() => {
-        if (audioContext) {
-            const audioSrc = audioContext.createMediaElementSource(audioElementRef.current);
-            setSource(audioSrc);
-            const biquadFilter = audioContext.createBiquadFilter();
-            setBandPassFilter(biquadFilter);
-        }
-    }, [audioContext])
-
-    useEffect(() => {
-        if (source) {
-            source.connect(bandpassFilter);
-            bandpassFilter.connect(audioContext.destination);
-        }
-    }, [source, bandpassFilter])
-
-    return (
-        <ThemeProvider theme={theme}>
-            {audioUrl && !playing && <Button
-                variant="condensed"
-                onClick={() => handleClick()}
-            >
-                Play Sample
-            </Button>}
-
-            <div>
-
-                {playing && <audio
-                    src={audioUrl}
-                    ref={audioElementRef}
-                    onTimeUpdate={handleTimeUpdate}
-                    controls
-                />}
-
-            </div>
-
-        </ThemeProvider>
-    );
+      <div>
+        {playing && (
+          <audio
+            src={audioUrl}
+            ref={audioElementRef}
+            onTimeUpdate={handleTimeUpdate}
+            controls
+          />
+        )}
+      </div>
+    </ThemeProvider>
+  );
 }
--- Frontend/src/components/SoundAnalysisTools/Spectrogram.jsx
+++ Frontend/src/components/SoundAnalysisTools/Spectrogram.jsx
@@ -1,89 +1,96 @@
-import Plot from 'react-plotly.js';
-import React, { useEffect, useState } from 'react';
+import Plot from "react-plotly.js";
+import React, { useEffect, useState, useRef } from "react";
 
+export default function Spectrogram({
+  xData,
+  yData,
+  zData,
+  colorscale,
+  xrange,
+  setXrange,
+  yrange,
+  setYrange,
+  currentTime,
+  fileName,
+}) {
+  // todo calculate zmin and zmax from data
+  //
+  // Define label for the vertical line
+  const [lineX, setLineX] = useState(null);
+  const [lineY, setLineY] = useState(null);
+  const [label, setLabel] = useState(null);
+  const plotRef = useRef(null);
+  const handleOnAfterPlot = () => {
+    let newXrange = plotRef.current.props.layout.xaxis.range;
+    let newYrange = plotRef.current.props.layout.yaxis.range;
+    setXrange(newXrange);
+    setYrange(newYrange);
+  };
+  useEffect(() => {
+    const label = {
+      text: `Current Time: ${currentTime.toFixed(2)} s`, // Customize label text as needed
+      x: currentTime,
+      y: Math.max(...yrange),
+      showarrow: true,
+      arrowhead: 0,
+      ax: 0,
+      ay: -30,
+    };
+    // Create x and y coordinates for the vertical line
+    const verticalLineX = [currentTime, currentTime];
+    const verticalLineY = [Math.min(...yrange), Math.max(...yrange)];
 
-
-
-
-export default function Spectrogram({ xData, yData, zData, colorscale, xrange, yrange, currentTime, fileName }) {
-
-    // todo calculate zmin and zmax from data
-    // 
-    // Define label for the vertical line
-    const [lineX, setLineX] = useState(null);
-    const [lineY, setLineY] = useState(null);
-    const [label, setLabel] = useState(null);
-
-    useEffect(() => {
-        const label = {
-            text: `Current Time: ${(currentTime).toFixed(2)} s`, // Customize label text as needed
-            x: currentTime,
-            y: Math.max(...yrange),
-            showarrow: true,
-            arrowhead: 0,
-            ax: 0,
-            ay: -30,
-        };
-        // Create x and y coordinates for the vertical line
-        const verticalLineX = [currentTime, currentTime];
-        const verticalLineY = [Math.min(...yrange), Math.max(...yrange)];
-
-        setLineX(verticalLineX)
-        setLineY(verticalLineY)
-        setLabel(label)
-
-    }, [currentTime])
-    return (
-        <Plot
-            data={[
-                {
-                    type: "heatmap",
-                    x: xData,
-                    y: yData,
-                    z: zData,
-                    colorscale: colorscale,
-                    connectgaps: true,
-                    ncontours: 500,
-                    hovertemplate: '<b>Time</b>: %{x} s<br><b>Frequency</b>: %{y} Hz<br><b>Amplitude</b>: %{z} dB',
-
-                },
-                {
-                    type: "scatter",
-                    mode: "lines",
-                    x: lineX,
-                    y: lineY,
-                    line: {
-                        color: "red", // Change color as needed
-                        width: 1,
-                        opacity: 0.7, // Set opacity to 70%
-                    },
-                },
-            ]}
-
-            layout={{
-                title: fileName + " Spectrogram",
-                xaxis: {
-                    title: "Time (s)",
-                    range: xrange
-
-                },
-                yaxis: {
-                    title: "Frequency (Hz)",
-                    range: yrange
-                },
-                plot_bgcolor: 'rgba(0, 0, 0, 0)', // Set plot background color to transparent for dark mode
-                paper_bgcolor: 'rgba(0, 0, 0, 0)', // Set paper background color to transparent for dark mode
-                font: {
-                    color: 'white', // Set font color to white for dark mode
-                },
-                annotations: [label]
-            }}
-            useResizeHandler={true}
-            style={{ width: "100%", height: "100%" }}
-
-
-        />
-
-
-    )
+    setLineX(verticalLineX);
+    setLineY(verticalLineY);
+    setLabel(label);
+  }, [currentTime]);
+  return (
+    <Plot
+      data={[
+        {
+          type: "heatmap",
+          x: xData,
+          y: yData,
+          z: zData,
+          colorscale: colorscale,
+          connectgaps: true,
+          ncontours: 300,
+          hovertemplate:
+            "<b>Time</b>: %{x} s<br><b>Frequency</b>: %{y} Hz<br><b>Amplitude</b>: %{z} dB",
+        },
+        {
+          type: "scatter",
+          mode: "lines",
+          x: lineX,
+          y: lineY,
+          line: {
+            color: "red", // Change color as needed
+            width: 1,
+            opacity: 0.7, // Set opacity to 70%
+          },
+        },
+      ]}
+      layout={{
+        title: fileName + " Spectrogram",
+        xaxis: {
+          title: "Time (s)",
+          range: xrange,
+        },
+        yaxis: {
+          title: "Frequency (Hz)",
+          range: yrange,
+        },
+        plot_bgcolor: "rgba(0, 0, 0, 0)", // Set plot background color to transparent for dark mode
+        paper_bgcolor: "rgba(0, 0, 0, 0)", // Set paper background color to transparent for dark mode
+        font: {
+          color: "white", // Set font color to white for dark mode
+        },
+        annotations: [label],
+      }}
+      useResizeHandler={true}
+      style={{ width: "100%", height: "100%" }}
+      onAfterPlot={handleOnAfterPlot}
+      ref={plotRef}
+    />
+  );
 }
