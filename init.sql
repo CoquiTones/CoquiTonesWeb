5f6e78befe1c6681fe44e0ef449df9d2bd0da6f0
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-10-17 00:28:39 -0700

    added sql to test dashboard endpoint

--- init.sql
+++ init.sql
@@ -53,5 +53,15 @@ VALUES
     ('secondary',18.4103,-66.0944,'Bosque San Patricio'),
     ('primary',18.2201,-66.5283,'Bosque Los Tres Picachos');
 
+INSERT INTO timestampindex (nid, ttime)
+    VALUES 
+        (1, TO_TIMESTAMP(
+    '2017-03-31 9:30:20',
+    'YYYY-MM-DD HH:MI:SS'));
+
+INSERT INTO weatherdata (tid, wdtemperature, wdhumidity, wdpressure, wddid_rain)
+VALUES
+    (1, 75.4, 45.5, 1000, true);
+
 
 
