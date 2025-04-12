442954ecba85bdf9629f76c14fd8f3bb3867fdbc
AlonsoUriarte14 <diego.santiago6@upr.edu>
2025-04-11 19:37:57 -0700

    updated scripts

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 mkdir log
-backend_logfile=`$(pwd)/log/backend.log`
-frontend_logfile=`$(pwd)/log/frontend.log`
-
+cwd=$(pwd)
+backend_logfile=${cwd}/log/backend.log
+frontend_logfile=${cwd}/log/frontend.log
+echo "backend log: ${backend_logfile}"
+touch ${backend_logfile}
+touch ${frontend_logfile}
 ## Database
-docker compose up
+docker compose down &
+docker compose up -d -build &
 echo "Database Started"
 ## Backend 
-uvicorn --app-dir Backend/src app:app --host 0.0.0.0 --port 8080 > ${backend_logfile}
-echo "Backend Started. To monitor use tail -f log/backend.log"
+uvicorn --app-dir Backend/src app:app --host 0.0.0.0 --port 8080 > ${backend_logfile} 2>&1 &
+backend_pid=$!
+
+echo "Backend Started. PID: ${backend_pid} To monitor use tail -f log/backend.log or opening them up in vscode"
 
 ## Frontend
 cd Frontend/
-npm run start > ${frontend_logfile}
-echo "Frontend Started. To monitor use tail -f log/frontend.log" 
+npm run start > ${frontend_logfile} 2>&1 &
+frontend_pid=$! 
+echo "Frontend Started.  PID: ${frontend_pid}To monitor use tail -f log/frontend.log or opening them up in vscode" 
+
+cleanup () {
+    kill backend_pid
+    kill frontend_pid
+}
+
+trap cleanup SIGINT 
