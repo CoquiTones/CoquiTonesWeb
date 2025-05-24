996a9d803b4430fbf4ca8c58a8ccaac9a06bdc7f
AlonsoUriarte14 <diego.santiago6@upr.edu>
2025-05-23 22:34:45 -0700

    - add docker compose down on interrupt for better cleanup - removed version of docker compose yaml

--- scripts/docker_deployment.sh
+++ scripts/docker_deployment.sh
@@ -12,7 +12,7 @@ start_coquitones() {
 
 tail_logs() {
     LOG_DIR="log"
-    mkdir $LOG_DIR
+    mkdir -p $LOG_DIR
     for name in $(docker compose ps -q | xargs docker inspect --format '{{.Name}}' | sed 's|/||'); 
     do
         docker logs -f "$name" > "${LOG_DIR}/${name}.log" 2>&1 &
@@ -21,11 +21,26 @@ tail_logs() {
     docker compose logs -f frontend
 }
 
+# === Cleanup handler ===
+cleanup() {
+    docker compose down && echo "Database stopped"
+    exit 0
+}
+
+trap cleanup SIGINT
 
 if [$# -ne 0 ]; then
     usage
 fi
 
 start_coquitones
+
+# === Instructions ===
+echo
+echo "🌐 App is running!"
+echo "🛑 Press Ctrl+C to stop all services."
+
+
+
 tail_logs
 
