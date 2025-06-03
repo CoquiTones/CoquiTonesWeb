25de22f7ff06e72341d872b26cd35c2cf40f756b
Edwin Camuy <edwin.camuy@upr.edu>
2025-06-03 09:52:17 -0400

    Updated sql init to new version

--- init.sql
+++ init.sql
@@ -1,22 +1,17 @@
-DROP TYPE IF EXISTS node_type;
-
-DROP TABLE IF EXISTS node;
-
-DROP TABLE IF EXISTS timestampindex;
-
-DROP TABLE IF EXISTS audiofile;
-
-DROP TABLE IF EXISTS weatherdata;
-
-DROP TABLE IF EXISTS classifierreport;
+DROP TYPE IF EXISTS node_type CASCADE;
+DROP TABLE IF EXISTS node CASCADE;
+DROP TABLE IF EXISTS timestampindex CASCADE;
+DROP TABLE IF EXISTS audiofile CASCADE;
+DROP TABLE IF EXISTS weatherdata CASCADE;
+DROP TABLE IF EXISTS classifierreport CASCADE;
 
 CREATE TYPE node_type AS ENUM ('primary', 'secondary');
 
 CREATE TABLE node (
-    nid SERIAL PRIMARY KEY,
-    ntype node_type NOT NULL,
-    nlatitude REAL NOT NULL,
-    nlongitude REAL NOT NULL,
+    nid         SERIAL PRIMARY KEY,
+    ntype       node_type NOT NULL,
+    nlatitude   REAL NOT NULL,
+    nlongitude  REAL NOT NULL,
     ndescription VARCHAR(512)
 );
 
@@ -36,44 +31,25 @@ CREATE TABLE classifierreport (
     crno_hit            INTEGER NOT NULL
 );
 
-CREATE TABLE weatherdata (
-    wdid SERIAL PRIMARY KEY,
-    tid INTEGER REFERENCES timestampindex,
-    nid INTEGER REFERENCES node,
-    wdtemperature REAL,
-    wdhumidity REAL,
-    wdpressure REAL,
-    wddid_rain bool
+CREATE TABLE audiofile (
+    afid        SERIAL PRIMARY KEY,
+    tid         INTEGER REFERENCES  timestampindex,
+    nid         INTEGER REFERENCES node,
+    data        bytea NOT NULL
 );
 
-INSERT INTO
-    node (
-        ntype,
-        nlatitude,
-        nlongitude,
-        ndescription
-    )
-VALUES (
-        'primary',
-        18.0814,
-        -66.9038,
-        'Bosque Susua'
-    ),
-    (
-        'primary',
-        18.1504,
-        -66.9869,
-        'Bosque Estatal Maricao'
-    ),
-    (
-        'secondary',
-        18.4103,
-        -66.0944,
-        'Bosque San Patricio'
-    ),
-    (
-        'primary',
-        18.2201,
-        -66.5283,
-        'Bosque Los Tres Picachos'
-    );
+CREATE TABLE weatherdata (
+    wdid            SERIAL PRIMARY KEY,
+    tid             INTEGER REFERENCES timestampindex,
+    wdtemperature   REAL,
+    wdhumidity      REAL,
+    wdpressure      REAL,
+    wddid_rain      bool
+);
+
+INSERT INTO node (ntype, nlatitude, nlongitude, ndescription)
+VALUES 
+    ('primary',18.0814,-66.9038,'Bosque Susua'),
+    ('primary',18.1504,-66.9869,'Bosque Estatal Maricao'),
+    ('secondary',18.4103,-66.0944,'Bosque San Patricio'),
+    ('primary',18.2201,-66.5283,'Bosque Los Tres Picachos');
