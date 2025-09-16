563fafec93de424c2d71ef4ce7aabf4b3e203f24
EdwinC1339 <edwin.camuy@upr.edu>
2025-09-16 07:57:10 -0400

    Added username field and removed dummy data from init sql script

--- init.sql
+++ init.sql
@@ -9,6 +9,7 @@ CREATE TYPE node_type AS ENUM ('primary', 'secondary');
 
 CREATE TABLE appuser (
     auid        SERIAL PRIMARY KEY,
+    username    VARCHAR(30) NOT NULL,
     pwhash      VARCHAR(100) NOT NULL,
 )
 
@@ -57,10 +58,3 @@ CREATE TABLE weatherdata (
     wdpressure      REAL,
     wddid_rain      bool
 );
-
-INSERT INTO node (ntype, nlatitude, nlongitude, ndescription)
-VALUES 
-    ('primary',18.0814,-66.9038,'Bosque Susua'),
-    ('primary',18.1504,-66.9869,'Bosque Estatal Maricao'),
-    ('secondary',18.4103,-66.0944,'Bosque San Patricio'),
-    ('primary',18.2201,-66.5283,'Bosque Los Tres Picachos');
