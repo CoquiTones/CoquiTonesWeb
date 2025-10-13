2d286ac5bfea46d621f3b65c0403ad1dfaea0115
AlonsoUriarte14 <diego.santiago6@upr.edu>
2025-10-13 19:25:10 -0400

    create sign up page and add link to sign up page from sign in modal

--- frontend/src/components/shared/SignInModal.jsx
+++ frontend/src/components/shared/SignInModal.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
-import { 
-    Modal, 
-    Box, 
-    Typography, 
-    TextField, 
-    Button,
-    Stack,
-    Divider, 
-    styled 
+import {
+	Modal,
+	Box,
+	Typography,
+	TextField,
+	Button,
+	Stack,
+	Divider,
+	styled,
+	Link
 } from '@mui/material';
 import logo from "../assets/images/logo512.png";
 import { APIHandlerAuthentication } from '../../services/rest/APIHandler/APIHandlerAuthentication';
@@ -16,132 +17,130 @@ import { AuthenticateUserRequest } from '../../services/rest/RequestORM/Authenti
 
 
 const ModalContainer = styled(Box)(({ theme }) => ({
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    backgroundColor: "#191716",
-    border: '2px solid #000',
-    boxShadow: 24,
-    padding: theme.spacing(4),
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	backgroundColor: "#191716",
+	border: '2px solid #000',
+	boxShadow: 24,
+	padding: theme.spacing(4),
 
-    '& img': {
-        width: '50%',
-        height: '50%',
-        display: 'flex',
-        justifyContent: 'center',
-        margin:'auto',
-    }
+	'& img': {
+		width: '50%',
+		height: '50%',
+		display: 'flex',
+		justifyContent: 'center',
+		margin: 'auto',
+	}
 }));
 
 const SignInModal = ({ open, setOpen }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-    const handleSignIn = async () => {
-    try {
-        
-        const handler = new APIHandlerAuthentication();
-        const checkUserRequest = new AuthenticateUserRequest(username, password);
-        const successfullyAuthenticatedStatus = await handler.setSessionTokenIfUserExists(checkUserRequest);
+	const handleSignIn = async () => {
+		try {
 
-        if (successfullyAuthenticatedStatus) {
-            alert('Successfully authenticated'); // obviously change this to something pretty 
-            handleClose();
-        }
-        else {
-            alert('incorrect username or password. Try again. ') // obviously change this to something pretty 
-        }
-        } catch (error) {
-      // Handle authentication errors
-        console.error('Sign-in error:', error);
-    }
-};
+			const handler = new APIHandlerAuthentication();
+			const checkUserRequest = new AuthenticateUserRequest(username, password);
+			const successfullyAuthenticatedStatus = await handler.setSessionTokenIfUserExists(checkUserRequest);
 
-    const handleSignUpClick = () => {
-        handleClose();
-        // onSignUp && onSignUp();
-    }
+			if (successfullyAuthenticatedStatus) {
+				alert('Successfully authenticated'); // obviously change this to something pretty 
+				handleClose();
+			}
+			else {
+				alert('incorrect username or password. Try again. ') // obviously change this to something pretty 
+			}
+		} catch (error) {
+			// Handle authentication errors
+			console.error('Sign-in error:', error);
+		}
+	};
 
-    const handleClose =  () => {
-        
-        setOpen(!open)
-    }
-return (
-    <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="sign-in-modal-title"
-    >
-        <ModalContainer>
-            <img src={logo}></img>
-        <Typography 
-        id="sign-in-modal-title" 
-        variant="h6" 
-        component="h2" 
-        align='center'
-        sx={{
-            color:"#ffc857",
-            cursor: 'pointer',
-            fontSize:'1.5rem',
-            fontWeight: 'bold',
-            TextDecoration:'none'
-        }}>
-            Sign In
-        </Typography>
-        <TextField
-            fullWidth
-            margin="normal"
-            label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextField
-            fullWidth
-            margin="normal"
-            type="password"
-            label="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
 
-        <Stack spacing={2} sx={{mt: 2}}>
-            <Button 
-            variant="contained" 
-            onClick={handleSignIn}
-            fullWidth
-            sx={{ mt: 2 }}
-        >
-            Sign In
-            </Button>
-            <Divider>
-            <Typography variant="body2" color="text.secondary">
-                OR
-            </Typography>
-            </Divider>
 
-            <Button 
-            variant="outlined" 
-            color="secondary"
-            onClick={handleSignUpClick}
-            fullWidth
-            >
-            Sign Up
-            </Button>
+	const handleClose = () => {
 
-            <Button 
-            variant="text" 
-            color="error"
-            onClick={handleClose}
-            fullWidth
-            >
-            Dismiss
-            </Button>
-        </Stack>
-        </ModalContainer>
-    </Modal>
-    );
+		setOpen(!open)
+	}
+	return (
+		<Modal
+			open={open}
+			onClose={handleClose}
+			aria-labelledby="sign-in-modal-title"
+		>
+			<ModalContainer>
+				<img src={logo}></img>
+				<Typography
+					id="sign-in-modal-title"
+					variant="h6"
+					component="h2"
+					align='center'
+					sx={{
+						color: "#ffc857",
+						cursor: 'pointer',
+						fontSize: '1.5rem',
+						fontWeight: 'bold',
+						TextDecoration: 'none'
+					}}>
+					Sign In
+				</Typography>
+				<TextField
+					fullWidth
+					margin="normal"
+					label="Username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<TextField
+					fullWidth
+					margin="normal"
+					type="password"
+					label="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+
+				<Stack spacing={2} sx={{ mt: 2 }}>
+					<Button
+						variant="contained"
+						onClick={handleSignIn}
+						fullWidth
+						sx={{ mt: 2 }}
+					>
+						Sign In
+					</Button>
+					<Divider>
+						<Typography variant="body2" color="text.secondary">
+							OR
+						</Typography>
+					</Divider>
+
+					<Button
+						variant="outlined"
+						color="secondary"
+						fullWidth
+					>
+						<Link href="/SignUp">
+							Sign Up
+						</Link>
+					</Button>
+
+					<Button
+						variant="text"
+						color="error"
+						onClick={handleClose}
+						fullWidth
+					>
+						Dismiss
+					</Button>
+				</Stack>
+			</ModalContainer>
+		</Modal>
+	);
 };
 
 export default SignInModal;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,96 +1,115 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import Cookies from 'js-cookie';
 import {
-    Modal,
-    Box,
-    Typography,
-    TextField,
-    Button,
-    Stack,
-    Divider,
-    styled
+	Modal,
+	Box,
+	Typography,
+	TextField,
+	Button,
+	Stack,
+	Divider,
+	styled,
+	CssBaseline,
+	Container,
+	Grid2
 } from '@mui/material';
+import Sidebar from "../components/shared/Sidebar";
+
 import logo from "../components/assets/images/logo512.png";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 import { APIHandlerAuthentication } from '../services/rest/APIHandler/APIHandlerAuthentication';
 import { SignUpRequest } from '../services/rest/RequestORM/Authentication/SignUpRequest';
 
-
 const SignUpPage = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [isOpen, setIsOpen] = useState(false);
+	const toggle = () => {
+		setIsOpen(!isOpen);
+	};
+	const handleSignUp = async () => {
+		try {
 
-    const handleSignUp = async () => {
-        try {
+			const handler = new APIHandlerAuthentication();
+			const signupRequest = new SignUpRequest(username, password);
+			const successfullyAuthenticatedStatus = await handler.SignUpNewUser(signupRequest);
 
-            const handler = new APIHandlerAuthentication();
-            const signupRequest = new SignUpRequest(username, password);
-            const successfullyAuthenticatedStatus = await handler.SignUpNewUser(signupRequest);
+			if (successfullyAuthenticatedStatus) {
+				alert('Successfully Signed Up!!'); // obviously change this to something pretty 
+				handleClose();
+			}
+			else {
+				alert('Some Fucking Error Happened!') // obviously change this to something pretty 
+			}
+		} catch (error) {
+			// Handle authentication errors
+			console.error('Sign-in error:', error);
+		}
+	};
 
-            if (successfullyAuthenticatedStatus) {
-                alert('Successfully Signed Up!!'); // obviously change this to something pretty 
-                handleClose();
-            }
-            else {
-                alert('Some Fucking Error Happened!') // obviously change this to something pretty 
-            }
-        } catch (error) {
-            // Handle authentication errors
-            console.error('Sign-in error:', error);
-        }
-    };
+	const handleClose = () => {
 
-    const handleClose = () => {
+		setOpen(!open)
+	}
+	return (
+		<Fragment>
 
-        setOpen(!open)
-    }
-    return (
+			<Sidebar isOpen={isOpen} toggle={toggle} />
+			<Navbar toggle={toggle} />
+			<Box sx={{ display: "flex", width: "100%" }}>
+				<CssBaseline />
+				<Container>
+					<Grid2>
+						<img src={logo}></img>
+						<Typography
+							id="sign-in-modal-title"
+							variant="h6"
+							component="h2"
+							align='center'
+							sx={{
+								color: "#ffc857",
+								cursor: 'pointer',
+								fontSize: '1.5rem',
+								fontWeight: 'bold',
+								TextDecoration: 'none'
+							}}>
+							Sign In
+						</Typography>
+						<TextField
+							fullWidth
+							margin="normal"
+							label="Username"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+						<TextField
+							fullWidth
+							margin="normal"
+							type="password"
+							label="Password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
 
-        <div>
+						<Stack spacing={2} sx={{ mt: 2 }}>
+							<Button
+								variant="contained"
+								onClick={handleSignUp}
+								fullWidth
+								sx={{ mt: 2 }}
+							>
+								Sign Up
+							</Button>
+						</Stack>
 
-            <img src={logo}></img>
-            <Typography
-                id="sign-in-modal-title"
-                variant="h6"
-                component="h2"
-                align='center'
-                sx={{
-                    color: "#ffc857",
-                    cursor: 'pointer',
-                    fontSize: '1.5rem',
-                    fontWeight: 'bold',
-                    TextDecoration: 'none'
-                }}>
-                Sign In
-            </Typography>
-            <TextField
-                fullWidth
-                margin="normal"
-                label="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <TextField
-                fullWidth
-                margin="normal"
-                type="password"
-                label="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
+					</Grid2>
+				</Container>
+			</Box>
+		</Fragment>
 
-            <Stack spacing={2} sx={{ mt: 2 }}>
-                <Button
-                    variant="contained"
-                    onClick={handleSignUp}
-                    fullWidth
-                    sx={{ mt: 2 }}
-                >
-                    Sign Up
-                </Button>
-            </Stack>
-        </div>
 
-    );
+	);
 };
 
 export default SignUpPage;
