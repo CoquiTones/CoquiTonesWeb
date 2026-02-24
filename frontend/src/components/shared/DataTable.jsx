65f6acd07ddc72be158b42aa2d9fef71ffd3dda1
Capitan Guajon <dasu.santiago@gmail.com>
2026-02-24 18:56:08 -0400

    fix: adding file to test python testing

---
+++ frontend/src/components/shared/DataTable.jsx
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+
+
+export default DataTable = () => {
+
+    const [pageSize, setPageSize] = useState(5);
+    const [searchString, setSearchString] = useState(null);
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
