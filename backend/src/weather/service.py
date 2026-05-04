d12a7f1d48ac955d0709d770ce2656efda6c4c77
EdwinC1339 <edwin.camuy@upr.edu>
2026-05-04 16:15:48 -0400

    Weather API cleanup

--- backend/src/weather/service.py
+++ backend/src/weather/service.py
@@ -2,23 +2,23 @@ from weather.repository import WeatherData
 
 from dbutil import DBTransactionDependency
 from routers.security import LightWeightUser, get_current_user
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import Depends, Form, APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/weather", tags=["Weather"])
 
-@router.get("/weather/all")
+@router.get("/all")
 async def weather_all(
     current_user: Annotated[LightWeightUser, Depends(get_current_user)],
     transaction: DBTransactionDependency,
-):
+) -> List[WeatherData]:
     return await WeatherData.get_all(current_user.auid, transaction.connection)
 
 
-@router.get("/weather/")
+@router.get("/")
 async def weather_get(
     wdid: Annotated[int, Form()],
     current_user: Annotated[LightWeightUser, Depends(get_current_user)],
     transaction: DBTransactionDependency,
-):
+) -> WeatherData | None:
     return await WeatherData.get(current_user.auid, wdid, transaction.connection)
