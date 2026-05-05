f69cea576d63ec8a0605e816a0337127b1af91ac
EdwinC1339 <edwin.camuy@upr.edu>
2026-05-04 22:57:43 -0400

    Timestamp endpoints organized

--- backend/src/timestamp/service.py
+++ backend/src/timestamp/service.py
@@ -1,23 +1,23 @@
 import timestamp.repository as repository
 from dbutil import DBTransactionDependency
 from routers.security import LightWeightUser, get_current_user
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import Depends, Form, APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/timestamp", tags=["Timestamp"])
 
-@router.get("/timestamp/all")
+@router.get("/all")
 async def timestamp_all(
     current_user: Annotated[LightWeightUser, Depends(get_current_user)],
     transaction: DBTransactionDependency,
-):
+) -> List[repository.TimestampIndex]:
     return await repository.TimestampIndex.get_all(current_user.auid, transaction.connection)
 
 
-@router.get("/timestamp")
+@router.get("/")
 async def timestamp_get(
     tid: Annotated[int, Form()],
     current_user: Annotated[LightWeightUser, Depends(get_current_user)],
     transaction: DBTransactionDependency,
-):
+) -> repository.TimestampIndex | None:
     return await repository.TimestampIndex.get(current_user.auid, tid, transaction.connection)
