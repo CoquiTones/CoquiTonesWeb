805b7c3f99ecc8ef4f86049d132e4afa552ce086
EdwinC1339 <edwin.camuy@upr.edu>
2026-05-08 17:43:52 -0400

    Added checks to operations that insert timestampindex to make them secure. Fixes #103

--- backend/src/audio/basic_service.py
+++ backend/src/audio/basic_service.py
@@ -1,11 +1,16 @@
 import audio.repository as repository
 
 from timestamp.repository import TimestampIndex
+from node.repository import Node
 
 from psycopg import AsyncConnection
 from mlutil import classify_audio_file
 from datetime import datetime
 from asyncio import TaskGroup
+
+from Logger import Logger
+
+LOGGER = Logger.getInstance("Basic Audio Service")
 
 async def classify_and_save(audio, audio_file_id, db, model):
     classifier_output = classify_audio_file(audio, model)
@@ -30,11 +35,18 @@ async def insert_audio_and_timestamp(
         file,
         nid: int,
         timestamp: datetime
-): 
-    async with TaskGroup() as setup_group:
-        tid = setup_group.create_task(TimestampIndex.insert(db, nid, timestamp))
-        data = setup_group.create_task(file.read())
+):
+    async with db.transaction(): 
+        async with TaskGroup() as setup_group:
+            # Check that nid is actually owned by the ownerid.
+            node = setup_group.create_task(Node.get(owner, nid, db))
+            tid = setup_group.create_task(TimestampIndex.insert(db, nid, timestamp))
+            data = setup_group.create_task(file.read())
 
+        if node.result() is None:
+            LOGGER.error(f"Attempted to save audio file created by node {nid} but that node does not belong to user {owner} or does not exist.")
+            raise ValueError # this rolls back the transaction.
+    
     audio_file_id = await repository.AudioFile.insert(db, data.result(), nid, tid.result())
     return audio_file_id
     
